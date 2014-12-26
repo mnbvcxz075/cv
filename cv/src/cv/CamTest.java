897061cb96fb4af016fa9049970e45872ef27208
mnbvcxz075 <x170gbc9j346yv2tbj7z@gmail.com>
2014-12-26 20:24:54 +0900

    make CamTest

---
+++ cv/src/cv/CamTest.java
@@ -0,0 +1,45 @@
+package cv;
+
+import javax.swing.JFrame;
+
+import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
+
+public class CamTest {
+
+    public static void main(String[] args) {
+        new CamTest().loop();
+    }
+
+    private void loop() {
+        CanvasFrame canvas = new CanvasFrame("Webcam");
+        canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // デフォルト(0)のWebカメラを使う
+        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+        try {
+            grabber.start();
+
+            // フレームレートを取得
+            double frameRate = grabber.getFrameRate();
+            long wait = (long) (1000 / (frameRate == 0 ? 10 : frameRate));
+
+            // 画像を取りつづける
+            while (true) {
+                Thread.sleep(wait);
+                IplImage image = grabber.grab();
+
+                // 取ってきた画像を画面に表示
+                if (image != null) {
+                    canvas.showImage(image);
+                }
+            }
+
+        // 何かあったらエラーを吐いて終わる
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
