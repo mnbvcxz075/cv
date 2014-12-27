528a9703e6dae83b1da6b8513497c201ebfeebdc
mnbvcxz075 <x170gbc9j346yv2tbj7z@gmail.com>
2014-12-27 17:29:37 +0900

    test

--- cv/src/cv/UseYCC.java
+++ cv/src/cv/UseYCC.java
@@ -38,6 +38,22 @@ public class UseYCC extends UseImage{
 		update();
 	}
 
+	@Override
+	public void run(){
+		try {
+			sleep(wait);
+
+			img = grabber.grab();
+
+			update();
+
+		} catch (InterruptedException | Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+
+
 	private void update(){
 		label.setText(max[0]+" "+max[1]+" "+max[2]);
 		label2.setText(min[0]+" "+min[1]+" "+min[2]);
