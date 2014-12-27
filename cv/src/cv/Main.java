6e640f541c83b0dacebf564e558d49a2c7df5904
mnbvcxz075 <x170gbc9j346yv2tbj7z@gmail.com>
2014-12-27 21:13:07 +0900

    test

--- cv/src/cv/Main.java
+++ cv/src/cv/Main.java
@@ -18,14 +18,14 @@ public class Main{
 			return;
 		}
 
-//		while(true){
-//			try {
-//				uimg.update();
-//			} catch (Exception e) {
-//				// TODO 自動生成された catch ブロック
-//				e.printStackTrace();
-//			}
-//		}
+		while(true){
+			try {
+				uimg.update();
+			} catch (Exception e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+		}
 
 	}
 
